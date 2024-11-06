66fe06dda5f5d30af88561a801d6ccddca36a093
RPi72 <Angelo.S.Arriaga@outlook.com>
2024-11-05 17:11:26 -0800

    Added main.c

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/***************
+ *
+ * Class:: CSC-615-01 Fall 2024
+ * Names:: Anisah Chowdhury
+ *         Angelo Arriaga
+ *         Owen Meyer
+ *         Austin Ng
+ *         Citlalin Hernandez
+ *
+ * Github-Name:: DeadMartyr
+ * Project:: Term Project
+ *
+ * File:: main.c
+ * Description:: The main file of the final term project
+ *
+ * *************/
