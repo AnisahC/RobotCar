86c34ae275bbe9283a6ae98799a68266ea15db35
RPi72 <Angelo.S.Arriaga@outlook.com>
2024-11-12 17:33:31 -0800

    started moving over relevant Assignment 4 stuff

--- src/main.c
+++ src/main.c
@@ -14,3 +14,28 @@
  * Description:: The main file of the final term project
  *
  * *************/
+
+
+#include <pigpio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define DEBUG_FLAG 0
+
+#define PIN_SENSOR_IR   13
+#define PIN_SENSOR_LINE 27
+
+#define MICROSECONDS_UNTIL_TERMINATE 4000000
+#define PERIOD_DISPLAY                100000
+#define PERIOD_SCAN                    25000
+
+/* Prototype Signatures for Sensors */
+void* t_sensor_line(void* arg);
+void* t_sensor_ir(void* arg);
+
+/* Global Timer */
+useconds_t microsecRemaining = MICROSECONDS_UNTIL_TERMINATE; 
