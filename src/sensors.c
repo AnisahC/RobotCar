00b9921960a7e6866af88c1b314c77b734b05d25
Martyr <Angelo.S.Arriaga@outlook.com>
2024-11-14 14:34:13 -0800

    Added header to sensors.c

--- src/sensors.c
+++ src/sensors.c
@@ -1,3 +1,20 @@
+/***************
+ *
+ * Class:: CSC-615-01 Fall 2024
+ * Names:: Anisah Chowdhury
+ *         Angelo Arriaga
+ *         Owen Meyer
+ *         Austin Ng
+ *         Citlalin Hernandez
+ *
+ * Github-Name:: DeadMartyr
+ * Project:: Term Project
+ *
+ * File:: sensors.c
+ * Description:: The file implementing the functions used to read data from sensors
+ *
+ * *************/
+
 #include "sensors.h"
 
 //void* t_sensor_line(void* arg){
